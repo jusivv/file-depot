33548e13a5ec1c9f965dc081bd8e330d5ff315bb
jusivv <jusivv@live.com>
2020-05-21 17:02:27 +0800

    modify CORS Filter in webapp add debug mode

--- fd-webapp/src/main/java/org/vvodes/fd/webapp/boot/ProfileCorsConfigSource.java
+++ fd-webapp/src/main/java/org/vvodes/fd/webapp/boot/ProfileCorsConfigSource.java
@@ -3,11 +3,14 @@ package org.vvodes.fd.webapp.boot;
 import org.coodex.util.Profile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class ProfileCorsConfigSource implements CorsConfigurationSource {
     private static Logger log = LoggerFactory.getLogger(ProfileCorsConfigSource.class);
@@ -16,30 +19,68 @@ public class ProfileCorsConfigSource implements CorsConfigurationSource {
 
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        printRequest(request, profile.getBool("debug", false));
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(profile.getBool("allow-credentials", true));
         corsConfiguration.setMaxAge(profile.getLong("max-age", 3600));
         String[] list = profile.getStrList("allowed-headers");
         if (list != null) {
-            corsConfiguration.setAllowedHeaders(Arrays.asList(list));
+            for (String s : list) {
+                corsConfiguration.addAllowedHeader(s);
+            }
         }
         list = profile.getStrList("allowed-methods");
         if (list != null) {
-            corsConfiguration.setAllowedMethods(Arrays.asList(list));
+            for (String s : list) {
+                corsConfiguration.addAllowedMethod(s.toUpperCase());
+            }
         }
         list = profile.getStrList("exposed-headers");
         if (list != null) {
-            corsConfiguration.setAllowedMethods(Arrays.asList(list));
+            for (String s : list) {
+                corsConfiguration.addExposedHeader(s);
+            }
         }
         String[] allowedOrigins = profile.getStrList("allowed-origins", ",", new String[]{"*"});
         String origin = request.getHeader("Origin");
         for (String s : allowedOrigins) {
             if (s.equals("*") || s.equalsIgnoreCase(origin)) {
-                log.debug("CORS Origin matched {} with {}", origin, s);
-                corsConfiguration.setAllowedOrigins(Arrays.asList(new String[]{origin}));
+                corsConfiguration.addAllowedOrigin(origin);
                 break;
             }
         }
         return corsConfiguration;
+    }
+
+    private void printRequest(HttpServletRequest request, boolean debug) {
+        if (debug) {
+            log.debug("Request URI: {}", request.getRequestURI());
+            log.debug("Request method: {}", request.getMethod());
+            ServletServerHttpRequest serverRequest = new ServletServerHttpRequest(request);
+            HttpHeaders httpHeaders = serverRequest.getHeaders();
+            for (Iterator<String> it = httpHeaders.keySet().iterator(); it.hasNext(); ) {
+                String key = it.next();
+                log.debug("Header: {} = {}", key, listToStr(httpHeaders.get(key)));
+            }
+            log.debug("CORS filter end.");
+        }
+    }
+
+    private String listToStr(List<?> list) {
+        StringBuilder sb = new StringBuilder(4);
+        boolean isFirst = true;
+        for (Object o : list) {
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                sb.append(",");
+            }
+            if (o instanceof String) {
+                sb.append(o);
+            } else {
+                sb.append(o.toString());
+            }
+        }
+        return sb.toString();
     }
 }
